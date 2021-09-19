825fcdb67feaa60113284135f040bac19bbff2da
Samarth Bhardwaj <samarthb5109@gmail.com>
2021-09-19 10:33:04 +0530

    added alert message

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var billAmount = document.querySelector("#bill-amt");
 var cashGiven = document.querySelector("#cash-gn");
 var checkButton = document.querySelector("#check-btn");
-var message = document.querySelector()
+var message = document.querySelector("#error-message");
 
  checkButton.addEventListener("click", function validateBillAndCashAmount() {
+     message.style.display = "none";
 
     if(billAmount.value > 0){
 
@@ -13,9 +14,16 @@ var message = document.querySelector()
 
         }
     }else{
-        
+       displayMessage("Amount should ge greater than 0");
+
     }
 
  
 
 } );
+
+function displayMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+
+}
