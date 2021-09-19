5d1c9b45b75d257feda4f7da07ab5f07cd3ddaf1
Samarth Bhardwaj <samarthb5109@gmail.com>
2021-09-19 11:30:13 +0530

    added: calculating no.of notes

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ var billAmount = document.querySelector("#bill-amt");
 var cashGiven = document.querySelector("#cash-gn");
 var checkButton = document.querySelector("#check-btn");
 var message = document.querySelector("#error-message");
+var noOfNotes = document.querySelectorAll(".no-of-notes");
+
+var notes   =   [2000, 500, 100, 20, 10, 5, 1];
 
  checkButton.addEventListener("click", function validateBillAndCashAmount() {
-     message.style.display = "none";
+    hideMessage();
 
     if(billAmount.value > 0){
 
         if(cashGiven.value >= billAmount.value){
-            var balanceAmount   =   cashGiven   -   billAmount;
+            var balanceAmount   =   cashGiven.value   -   billAmount.value;
+           
+            calculateBalanceAmount(balanceAmount);
 
 
+        } else{
+            displayMessage("Cash provided should be atleast equal to the bill amount ");
         }
     }else{
        displayMessage("Amount should ge greater than 0");
@@ -21,6 +28,22 @@ var message = document.querySelector("#error-message");
  
 
 } );
+
+
+function calculateBalanceAmount(balanceAmount){
+    for(var i =0; i< notes.length; i++){
+        var numberOfNOtes   =   Math.trunc(balanceAmount / notes[i]);
+        balanceAmount   %=   notes[i];
+        noOfNotes[i].innerText = numberOfNOtes;        
+
+
+    }
+
+}
+
+function hideMessage(){
+    message.style.display = "none";
+}
 
 function displayMessage(msg){
     message.style.display = "block";
